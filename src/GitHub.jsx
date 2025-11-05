5e99627eca1151785069f923b71e6d2b5dbb6832
miacias <miaciasullo@gmail.com>
2025-11-05 14:58:49 -0500

    removed dead links from Heroku apps; added YouTube demos where applicable

--- src/GitHub.jsx
+++ src/GitHub.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable react/jsx-no-target-blank */
-import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { altText } from './utils/altText';
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { altText } from "./utils/altText";
 
 const Project = ({ projectName, featured }) => {
   const [projectData, setProjectData] = useState(null);
+  const youtubeDemos = [
+    {
+      name: "Brew Buddies v2",
+      link: "https://youtu.be/2a89CTdvhDI",
+    },
+    {
+      name: "Hey Boo Boo",
+      link: "https://youtu.be/Hr_MiHfB8rQ",
+    },
+    {
+      name: "State Park Excursion",
+      link: "https://youtu.be/KO30hAY_lMc",
+    },
+  ];
 
   const formatDate = (isoDateString) => {
     const date = new Date(isoDateString);
@@ -31,7 +45,7 @@ const Project = ({ projectName, featured }) => {
           updatedDate: formatDate(data.updated_at),
         });
       } catch (err) {
-        console.error('Error fetching project data:', err);
+        console.error("Error fetching project data:", err);
       }
     };
     fetchRepo();
@@ -42,33 +56,66 @@ const Project = ({ projectName, featured }) => {
   }
 
   if (featured) {
+    const formattedName = projectName.split("-").join(" ");
+    const demoVideo = youtubeDemos.find((demo) => demo.name.toLowerCase() === formattedName.toLowerCase());
+
     return (
-      <div className='featured-card'>
-        <div className='project-header'>
-          <h3 className='section-header project-name'>{projectName.split('-').join(' ')}</h3>
-          <div className='links-container'>
-            <a 
-              href={projectData.deployed ? projectData.deployed : '#'} 
-              className={`project-link ${!projectData.deployed ? 'inactive' : ''}`}
-              target='_blank'
-            >{projectData.deployed ? 'Deployed' : ''}</a>
-            <a 
+      <div className="featured-card">
+        <div className="project-header">
+          <h3 className="section-header project-name">{formattedName}</h3>
+
+          <div className="links-container">
+            {projectData.deployed && (
+              <a
+                href={projectData.deployed}
+                className={`project-link`}
+                target="_blank"
+              >
+                Deployed
+              </a>
+            )}
+
+            {demoVideo && (
+              <a
+                href={demoVideo.link}
+                className="project-link"
+                target="_blank"
+              >
+                Demo
+              </a>
+            )}
+
+            <a
               href={projectData.code}
-              className={`project-code ${!projectData.code ? 'inactive' : ''}`}
-              target='_blank'
-            >GitHub</a>
+              className={`project-code ${!projectData.code ? "inactive" : ""}`}
+              target="_blank"
+            >
+              GitHub
+            </a>
           </div>
         </div>
-        <div className='project-info'>
-          <img className='project-img' src={`./assets/images/${projectName}.png`} alt={altText(projectName)}/>
-          <div className='project-text'>
-            <p className='description'>{projectData.description}</p>
-            <ul className='topics-container'>
+
+        <div className="project-info">
+          <img
+            className="project-img"
+            src={`./assets/images/${projectName}.png`}
+            alt={altText(projectName)}
+          />
+
+          <div className="project-text">
+            <p className="description">{projectData.description}</p>
+
+            <ul className="topics-container">
               {projectData.topics.map((topic, index) => {
-              return (<li className='topic' key={index}>{topic}</li>)
+                return (
+                  <li className="topic" key={index}>
+                    {topic}
+                  </li>
+                );
               })}
             </ul>
-            <div className='dates'>
+
+            <div className="dates">
               <p>Created: {projectData.createdDate}</p>
               <p>Last updated: {projectData.updatedDate}</p>
             </div>
@@ -77,33 +124,67 @@ const Project = ({ projectName, featured }) => {
       </div>
     );
   } else {
+    const formattedName = projectName.split("-").join(" ");
+    const demoVideo = youtubeDemos.find((demo) => demo.name.toLowerCase() === formattedName.toLowerCase());
+
     return (
-      <div className='project-card'>
-        <div className='project-header'>
-          <h3 className='section-header project-name'>{projectName.split('-').join(' ')}</h3>
-          <div className='links-container'>
-            <a 
-              href={projectData.deployed ? projectData.deployed : '#'} 
-              className={`project-link ${!projectData.deployed ? 'inactive' : ''}`}
-              target='_blank'
-            >{projectData.deployed ? 'Deployed' : ''}</a>
-            <a 
+      <div className="project-card">
+        <div className="project-header">
+          <h3 className="section-header project-name">
+            {formattedName}
+          </h3>
+
+          <div className="links-container">
+            {projectData.deployed && (
+              <a
+                href={projectData.deployed}
+                className={`project-link`}
+                target="_blank"
+              >
+                Deployed
+              </a>
+            )}
+
+            {demoVideo && (
+              <a
+                href={demoVideo.link}
+                className="project-link"
+                target="_blank"
+              >
+                Demo
+              </a>
+            )}
+
+            <a
               href={projectData.code}
-              className={`project-code ${!projectData.code ? 'inactive' : ''}`}
-              target='_blank'
-            >GitHub</a>
+              className={`project-code ${!projectData.code ? "inactive" : ""}`}
+              target="_blank"
+            >
+              GitHub
+            </a>
           </div>
         </div>
-        <div className='project-info'>
-          <img className='project-img' src={`./assets/images/${projectName}.png`} alt=''/>
-          <div className='project-text'>
-            <p className='description'>{projectData.description}</p>
-            <ul className='topics-container'>
+
+        <div className="project-info">
+          <img
+            className="project-img"
+            src={`./assets/images/${projectName}.png`}
+            alt=""
+          />
+          <div className="project-text">
+            <p className="description">{projectData.description}</p>
+
+            <ul className="topics-container">
               {projectData.topics.map((topic, index) => {
-              return (<li className='topic' key={index}>{topic}</li>)
+                return (
+                  <li className="topic" key={index}>
+                    {topic}
+                  </li>
+                );
               })}
             </ul>
-            <div className='dates'>
+
+            <div className="dates">
               <p>Created: {projectData.createdDate}</p>
               <p>Last updated: {projectData.updatedDate}</p>
             </div>
@@ -112,11 +193,11 @@ const Project = ({ projectName, featured }) => {
       </div>
     );
   }
-}
+};
 
 Project.propTypes = {
   projectName: PropTypes.string.isRequired,
   featured: PropTypes.bool,
-}
+};
 
 export default Project;
